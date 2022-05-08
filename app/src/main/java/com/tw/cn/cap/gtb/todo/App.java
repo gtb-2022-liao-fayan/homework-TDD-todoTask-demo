3aaf0acb5a1cdfb24ff7972b630ad9da8500c85b
elenliao <807217780>
2022-05-08 19:12:36 +0800

    [refactor] 重构常量抽取成Constants类

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -2,15 +2,9 @@ package com.tw.cn.cap.gtb.todo;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 public class App {
-
-    public static final String USERS_HOME_PATH = System.getProperty("user.home");
-    public static final String TODO_CONFIG_ROOT = ".todo";
-    public static final String TASKS_FILENAME = "tasks";
-    public static final Path TASK_FILE_PATH = Path.of(USERS_HOME_PATH, TODO_CONFIG_ROOT, TASKS_FILENAME);
 
     public static void main(String[] args) {
 
@@ -18,7 +12,7 @@ public class App {
 
     public List<String> run() {
         try {
-            return Files.readAllLines(TASK_FILE_PATH);
+            return Files.readAllLines(Constants.TASK_FILE_PATH);
         } catch (IOException e) {
             throw new TodoCannotReadFileException();
         }
