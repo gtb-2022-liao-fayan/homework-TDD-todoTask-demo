881578cce18722121062ad617a96813e12a11735
elenliao <807217780>
2022-05-13 12:32:09 +0800

    [refactor] extract Section

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -14,12 +14,8 @@ public class App {
     public List<String> run() {
         final List<Task> tasks = taskRepository.loadTasks();
         final List<String> result = new ArrayList<>();
-        result.add("# To be Done");
-        tasks.stream().filter(item -> !item.isCompleted()).map(Task::format).forEach(result::add);
-        result.add("# Completed");
-        tasks.stream().filter(Task::isCompleted).map(Task::format).forEach(result::add);
+        result.addAll(Section.tbd().format(tasks));
+        result.addAll(Section.completed().format(tasks));
         return result;
     }
-
-
 }
---
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/Section.java
@@ -0,0 +1,36 @@
+package com.tw.cn.cap.gtb.todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Section {
+    private final String title;
+    private final boolean flag;
+
+    public Section(String title, boolean flag) {
+        this.title = title;
+        this.flag = flag;
+    }
+
+    static Section completed() {
+        return new Section("# Completed", true);
+    }
+
+    static Section tbd() {
+        return new Section("# To be Done", false);
+    }
+
+    List<String> format(List<Task> tasks) {
+        final List<String> result = new ArrayList<>();
+        result.add(title);
+        tasks.stream().
+                filter(this::isTypeMatched).
+                map(Task::format).
+                forEach(result::add);
+        return result;
+    }
+
+    private boolean isTypeMatched(Task item) {
+        return flag == item.isCompleted();
+    }
+}
