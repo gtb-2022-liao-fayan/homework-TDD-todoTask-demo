6da299e72d8904c372e5219ca2f15d44060ca9f1
elenliao <807217780>
2022-05-13 16:03:48 +0800

    [feat] throw Error

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -7,7 +7,7 @@ public class App {
     private final ListCommand listCommand = new ListCommand();
 
     public static void main(String[] args) {
-
+        throw new UnsupportedOperationException();
     }
 
     public List<String> run() {
