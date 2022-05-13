e2772b3d7ce5901e252a827fbf37e0db79207b4a
elenliao <807217780>
2022-05-13 15:55:19 +0800

    [feat] add test for complicated task name

--- app/src/test/java/com/tw/cn/cap/gtb/todo/TaskFactoryTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/TaskFactoryTest.java
@@ -3,12 +3,19 @@ package com.tw.cn.cap.gtb.todo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class TaskFactoryTest {
     @Test
     void should_parse_completed_property_for_task() {
-        final var isCompleted = TaskFactory.createTask(1,"+ foobar").isCompleted();
+        final var isCompleted = TaskFactory.createTask(1, "+ foobar").isCompleted();
         assertFalse(isCompleted);
+    }
+
+    @Test
+    void should_support_name_with_multiple_white_spaces() {
+        final var task = TaskFactory.createTask(1, "+        foo     bar").getName();
+        assertEquals("       foo     bar", task);
     }
 }
