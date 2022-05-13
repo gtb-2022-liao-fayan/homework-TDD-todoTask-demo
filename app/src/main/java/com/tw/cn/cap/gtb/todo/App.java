f8a9917f26c4d8a90e9d55d5fc95940a9aa7f4dd
elenliao <807217780>
2022-05-13 15:59:49 +0800

    [refactor] extract ListCommand

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -1,21 +1,17 @@
 package com.tw.cn.cap.gtb.todo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class App {
 
-    private final TaskRepository taskRepository = new TaskRepository();
+    private final ListCommand listCommand = new ListCommand();
 
     public static void main(String[] args) {
 
     }
 
     public List<String> run() {
-        final List<Task> tasks = taskRepository.loadTasks();
-        final List<String> result = new ArrayList<>();
-        result.addAll(Section.tbd().format(tasks));
-        result.addAll(Section.completed().format(tasks));
-        return result;
+        return listCommand.run();
     }
+
 }
